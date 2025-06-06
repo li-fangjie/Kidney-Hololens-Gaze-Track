f87f2d0790db5cc12a772764df55c21456aabbc7
Fangjie Li <li.fangjie2001@gmail.com>
2025-06-06 14:07:08 -0500

    Working Version with 30 Hz Default Gaze Track, 60 Hz fixed Update Rate, gaze sync with fixed update, Plane Based Track

--- Assets/Scripts/ExtendedEyeGazeDataProvider.cs
+++ Assets/Scripts/ExtendedEyeGazeDataProvider.cs
@@ -1,229 +1,232 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
+﻿//// Copyright (c) Microsoft Corporation.
+//// Licensed under the MIT license.
 
-using UnityEngine;
-using System;
-using Microsoft.MixedReality.OpenXR;
-using Microsoft.MixedReality.EyeTracking;
+//using UnityEngine;
+//using System;
+//using Microsoft.MixedReality.OpenXR;
+//using Microsoft.MixedReality.EyeTracking;
 
-/// <summary>
-/// This class provides access to the Extended Eye Gaze Tracking API 
-/// Values are given in Unity world space or relative to the main camera
-/// </summary>
-[DisallowMultipleComponent]
-public class ExtendedEyeGazeDataProvider : MonoBehaviour
-{
-    public enum GazeType
-    {
-        Left,
-        Right,
-        Combined
-    }
+///// <summary>
+///// This class provides access to the Extended Eye Gaze Tracking API 
+///// Values are given in Unity world space or relative to the main camera
+///// </summary>
+//[DisallowMultipleComponent]
+//public class ExtendedEyeGazeDataProvider : MonoBehaviour
+//{
 
-    public struct GazeReading
-    {
-        public bool IsValid;
-        public Vector3 EyePosition;
-        public Vector3 GazeDirection;
-        public GazeReading(bool isValid, Vector3 position, Vector3 direction)
-        {
-            IsValid = isValid;
-            EyePosition = position;
-            GazeDirection = direction;
-        }
-    }
+//}
+//{
+//    public enum GazeType
+//    {
+//        Left,
+//        Right,
+//        Combined
+//    }
 
-    private Camera _mainCamera;
-    private EyeGazeTrackerWatcher _watcher;
-    private EyeGazeTracker _eyeGazeTracker;
-    private EyeGazeTrackerReading _eyeGazeTrackerReading;
-    private System.Numerics.Vector3 _trackerSpaceGazeOrigin;
-    private System.Numerics.Vector3 _trackerSpaceGazeDirection;
-    private GazeReading _gazeReading;
-    private GazeReading _invalidGazeReading = new GazeReading(false, Vector3.zero, Vector3.zero);
-    private bool _gazePermissionEnabled;
-    private bool _readingSucceeded;
-    private SpatialGraphNode _eyeGazeTrackerNode;
-    private Pose _eyeGazeTrackerPose;
-    private Matrix4x4 _eyeGazeTrackerSpaceToPlayspace = new Matrix4x4();
-    private Matrix4x4 _eyeGazeTrackerSpaceToWorld = new Matrix4x4();
-    private Transform _mixedRealityPlayspace;
+//    public struct GazeReading
+//    {
+//        public bool IsValid;
+//        public Vector3 EyePosition;
+//        public Vector3 GazeDirection;
+//        public GazeReading(bool isValid, Vector3 position, Vector3 direction)
+//        {
+//            IsValid = isValid;
+//            EyePosition = position;
+//            GazeDirection = direction;
+//        }
+//    }
 
-    /// <summary>
-    /// Get the current reading for the requested GazeType, relative to the main camera
-    /// Will return a GazeReading with IsValid set to false if unable to return a valid reading
-    /// </summary>
-    /// <param name="gazeType"></param>
-    /// <returns></returns>
-    public GazeReading GetCameraSpaceGazeReading(GazeType gazeType)
-    {
-        return GetCameraSpaceGazeReading(gazeType, DateTime.Now);
-    }
+//    private Camera _mainCamera;
+//    private EyeGazeTrackerWatcher _watcher;
+//    private EyeGazeTracker _eyeGazeTracker;
+//    private EyeGazeTrackerReading _eyeGazeTrackerReading;
+//    private System.Numerics.Vector3 _trackerSpaceGazeOrigin;
+//    private System.Numerics.Vector3 _trackerSpaceGazeDirection;
+//    private GazeReading _gazeReading;
+//    private GazeReading _invalidGazeReading = new GazeReading(false, Vector3.zero, Vector3.zero);
+//    private bool _gazePermissionEnabled;
+//    private bool _readingSucceeded;
+//    private SpatialGraphNode _eyeGazeTrackerNode;
+//    private Pose _eyeGazeTrackerPose;
+//    private Matrix4x4 _eyeGazeTrackerSpaceToPlayspace = new Matrix4x4();
+//    private Matrix4x4 _eyeGazeTrackerSpaceToWorld = new Matrix4x4();
+//    private Transform _mixedRealityPlayspace;
 
-    /// <summary>
-    /// Get the reading for the requested GazeType at the given TimeStamp, relative to the main camera
-    /// Will return a GazeReading with IsValid set to false if unable to return a valid reading
-    /// </summary>
-    /// <param name="gazeType"></param>
-    /// <param name="timestamp"></param>
-    /// <returns></returns>
-    public GazeReading GetCameraSpaceGazeReading(GazeType gazeType, DateTime timestamp)
-    {
-        _gazeReading = GetWorldSpaceGazeReading(gazeType, timestamp);
-        if (!_gazeReading.IsValid)
-        {
-            return _invalidGazeReading;
-        }
+//    /// <summary>
+//    /// Get the current reading for the requested GazeType, relative to the main camera
+//    /// Will return a GazeReading with IsValid set to false if unable to return a valid reading
+//    /// </summary>
+//    /// <param name="gazeType"></param>
+//    /// <returns></returns>
+//    public GazeReading GetCameraSpaceGazeReading(GazeType gazeType)
+//    {
+//        return GetCameraSpaceGazeReading(gazeType, DateTime.Now);
+//    }
 
-        _gazeReading.EyePosition = _mainCamera.transform.InverseTransformPoint(_gazeReading.EyePosition);
-        _gazeReading.GazeDirection = _mainCamera.transform.InverseTransformDirection(_gazeReading.GazeDirection).normalized;
-        _gazeReading.IsValid = true;
+//    /// <summary>
+//    /// Get the reading for the requested GazeType at the given TimeStamp, relative to the main camera
+//    /// Will return a GazeReading with IsValid set to false if unable to return a valid reading
+//    /// </summary>
+//    /// <param name="gazeType"></param>
+//    /// <param name="timestamp"></param>
+//    /// <returns></returns>
+//    public GazeReading GetCameraSpaceGazeReading(GazeType gazeType, DateTime timestamp)
+//    {
+//        _gazeReading = GetWorldSpaceGazeReading(gazeType, timestamp);
+//        if (!_gazeReading.IsValid)
+//        {
+//            return _invalidGazeReading;
+//        }
 
-        return _gazeReading;
-    }
+//        _gazeReading.EyePosition = _mainCamera.transform.InverseTransformPoint(_gazeReading.EyePosition);
+//        _gazeReading.GazeDirection = _mainCamera.transform.InverseTransformDirection(_gazeReading.GazeDirection).normalized;
+//        _gazeReading.IsValid = true;
 
-    /// <summary>
-    /// Get the current reading for the requested GazeType
-    /// Will return a GazeReading with IsValid set to false if unable to return a valid reading
-    /// </summary>
-    /// <param name="gazeType"></param>
-    /// <returns></returns>
-    public GazeReading GetWorldSpaceGazeReading(GazeType gazeType)
-    {
-        return GetWorldSpaceGazeReading(gazeType, DateTime.Now);
-    }
+//        return _gazeReading;
+//    }
 
-    /// <summary>
-    /// Get the reading for the requested GazeType at the given TimeStamp
-    /// Will return a GazeReading with IsValid set to false if unable to return a valid reading
-    /// </summary>
-    /// <param name="gazeType"></param>
-    /// <param name="timestamp"></param>
-    /// <returns></returns>
-    public GazeReading GetWorldSpaceGazeReading(GazeType gazeType, DateTime timestamp)
-    {
-        if (!_gazePermissionEnabled || _eyeGazeTracker == null)
-        {
-            return _invalidGazeReading;
-        }
+//    /// <summary>
+//    /// Get the current reading for the requested GazeType
+//    /// Will return a GazeReading with IsValid set to false if unable to return a valid reading
+//    /// </summary>
+//    /// <param name="gazeType"></param>
+//    /// <returns></returns>
+//    public GazeReading GetWorldSpaceGazeReading(GazeType gazeType)
+//    {
+//        return GetWorldSpaceGazeReading(gazeType, DateTime.Now);
+//    }
 
-        _eyeGazeTrackerReading = _eyeGazeTracker.TryGetReadingAtTimestamp(timestamp);
-        if (_eyeGazeTrackerReading == null)
-        {
-            Debug.LogWarning($"Unable to get eyeGazeTrackerReading at: {timestamp.ToLongTimeString()}");
-            return _invalidGazeReading;
-        }
+//    /// <summary>
+//    /// Get the reading for the requested GazeType at the given TimeStamp
+//    /// Will return a GazeReading with IsValid set to false if unable to return a valid reading
+//    /// </summary>
+//    /// <param name="gazeType"></param>
+//    /// <param name="timestamp"></param>
+//    /// <returns></returns>
+//    public GazeReading GetWorldSpaceGazeReading(GazeType gazeType, DateTime timestamp)
+//    {
+//        if (!_gazePermissionEnabled || _eyeGazeTracker == null)
+//        {
+//            return _invalidGazeReading;
+//        }
 
-        _readingSucceeded = false;
-        switch (gazeType)
-        {
-            case GazeType.Left:
-                {
-                    _readingSucceeded = _eyeGazeTrackerReading.TryGetLeftEyeGazeInTrackerSpace(out _trackerSpaceGazeOrigin, out _trackerSpaceGazeDirection);
-                    break;
-                }
-            case GazeType.Right:
-                {
-                    _readingSucceeded = _eyeGazeTrackerReading.TryGetRightEyeGazeInTrackerSpace(out _trackerSpaceGazeOrigin, out _trackerSpaceGazeDirection);
-                    break;
-                }
-            case GazeType.Combined:
-                {
-                    _readingSucceeded = _eyeGazeTrackerReading.TryGetCombinedEyeGazeInTrackerSpace(out _trackerSpaceGazeOrigin, out _trackerSpaceGazeDirection);
-                    break;
-                }
-        }
-        if (!_readingSucceeded)
-        {
-            return _invalidGazeReading;
-        }
+//        _eyeGazeTrackerReading = _eyeGazeTracker.TryGetReadingAtTimestamp(timestamp);
+//        if (_eyeGazeTrackerReading == null)
+//        {
+//            Debug.LogWarning($"Unable to get eyeGazeTrackerReading at: {timestamp.ToLongTimeString()}");
+//            return _invalidGazeReading;
+//        }
 
-        // Get tracker pose under _mixedRealityPlayspace, and construct the matrix to transform gaze data from tracker space to _mixedRealityPlayspace
-        if (!_eyeGazeTrackerNode.TryLocate(_eyeGazeTrackerReading.SystemRelativeTime.Ticks, out _eyeGazeTrackerPose))
-        {
-            return _invalidGazeReading;
-        }
-        _eyeGazeTrackerSpaceToPlayspace.SetTRS(_eyeGazeTrackerPose.position, _eyeGazeTrackerPose.rotation, Vector3.one);
+//        _readingSucceeded = false;
+//        switch (gazeType)
+//        {
+//            case GazeType.Left:
+//                {
+//                    _readingSucceeded = _eyeGazeTrackerReading.TryGetLeftEyeGazeInTrackerSpace(out _trackerSpaceGazeOrigin, out _trackerSpaceGazeDirection);
+//                    break;
+//                }
+//            case GazeType.Right:
+//                {
+//                    _readingSucceeded = _eyeGazeTrackerReading.TryGetRightEyeGazeInTrackerSpace(out _trackerSpaceGazeOrigin, out _trackerSpaceGazeDirection);
+//                    break;
+//                }
+//            case GazeType.Combined:
+//                {
+//                    _readingSucceeded = _eyeGazeTrackerReading.TryGetCombinedEyeGazeInTrackerSpace(out _trackerSpaceGazeOrigin, out _trackerSpaceGazeDirection);
+//                    break;
+//                }
+//        }
+//        if (!_readingSucceeded)
+//        {
+//            return _invalidGazeReading;
+//        }
 
-        // Construct the matrix to transform gaze data from tracker space to Unity world space
-        _eyeGazeTrackerSpaceToWorld = (_mixedRealityPlayspace != null) ?
-                _mixedRealityPlayspace.localToWorldMatrix * _eyeGazeTrackerSpaceToPlayspace :
-                _eyeGazeTrackerSpaceToPlayspace;
+//        // Get tracker pose under _mixedRealityPlayspace, and construct the matrix to transform gaze data from tracker space to _mixedRealityPlayspace
+//        if (!_eyeGazeTrackerNode.TryLocate(_eyeGazeTrackerReading.SystemRelativeTime.Ticks, out _eyeGazeTrackerPose))
+//        {
+//            return _invalidGazeReading;
+//        }
+//        _eyeGazeTrackerSpaceToPlayspace.SetTRS(_eyeGazeTrackerPose.position, _eyeGazeTrackerPose.rotation, Vector3.one);
 
-        // Transform gaze data from tracker space to Unity world space 
-        _gazeReading.EyePosition = _eyeGazeTrackerSpaceToWorld.MultiplyPoint3x4(ToUnity(_trackerSpaceGazeOrigin));
-        _gazeReading.GazeDirection = _eyeGazeTrackerSpaceToWorld.MultiplyVector(ToUnity(_trackerSpaceGazeDirection));
-        _gazeReading.IsValid = true;
-        return _gazeReading;
-    }
+//        // Construct the matrix to transform gaze data from tracker space to Unity world space
+//        _eyeGazeTrackerSpaceToWorld = (_mixedRealityPlayspace != null) ?
+//                _mixedRealityPlayspace.localToWorldMatrix * _eyeGazeTrackerSpaceToPlayspace :
+//                _eyeGazeTrackerSpaceToPlayspace;
 
-    private async void Start()
-    {
-        _mainCamera = Camera.main;
-        _mixedRealityPlayspace = _mainCamera.transform.parent;
+//        // Transform gaze data from tracker space to Unity world space 
+//        _gazeReading.EyePosition = _eyeGazeTrackerSpaceToWorld.MultiplyPoint3x4(ToUnity(_trackerSpaceGazeOrigin));
+//        _gazeReading.GazeDirection = _eyeGazeTrackerSpaceToWorld.MultiplyVector(ToUnity(_trackerSpaceGazeDirection));
+//        _gazeReading.IsValid = true;
+//        return _gazeReading;
+//    }
 
-        Debug.Log("Initializing ExtendedEyeTracker");
-#if ENABLE_WINMD_SUPPORT
-        Debug.Log("Triggering eye gaze permission request");
-        // This function call may not required if you already use MRTK in your project 
-        _gazePermissionEnabled = await AskForEyePosePermission();
-#else
-        // Always enable when running in editor
-        _gazePermissionEnabled = true;
-#endif
+//    private async void Start()
+//    {
+//        _mainCamera = Camera.main;
+//        _mixedRealityPlayspace = _mainCamera.transform.parent;
 
-        if (!_gazePermissionEnabled)
-        {
-            Debug.LogError("Gaze is disabled");
-            return;
-        }
+//        Debug.Log("Initializing ExtendedEyeTracker");
+//#if ENABLE_WINMD_SUPPORT
+//        Debug.Log("Triggering eye gaze permission request");
+//        // This function call may not required if you already use MRTK in your project 
+//        _gazePermissionEnabled = await AskForEyePosePermission();
+//#else
+//        // Always enable when running in editor
+//        _gazePermissionEnabled = true;
+//#endif
 
-        _watcher = new EyeGazeTrackerWatcher();
-        _watcher.EyeGazeTrackerAdded += _watcher_EyeGazeTrackerAdded;
-        _watcher.EyeGazeTrackerRemoved += _watcher_EyeGazeTrackerRemoved;
-        await _watcher.StartAsync();
-    }
+//        if (!_gazePermissionEnabled)
+//        {
+//            Debug.LogError("Gaze is disabled");
+//            return;
+//        }
 
-    private void _watcher_EyeGazeTrackerRemoved(object sender, EyeGazeTracker e)
-    {
-        Debug.Log("EyeGazeTracker removed");
-        _eyeGazeTracker = null;
-    }
+//        _watcher = new EyeGazeTrackerWatcher();
+//        _watcher.EyeGazeTrackerAdded += _watcher_EyeGazeTrackerAdded;
+//        _watcher.EyeGazeTrackerRemoved += _watcher_EyeGazeTrackerRemoved;
+//        await _watcher.StartAsync();
+//    }
 
-    private async void _watcher_EyeGazeTrackerAdded(object sender, EyeGazeTracker e)
-    {
-        Debug.Log("EyeGazeTracker added");
-        try
-        {
-            await e.OpenAsync(true);
-            _eyeGazeTracker = e;
-            var supportedFrameRates = _eyeGazeTracker.SupportedTargetFrameRates;
-            foreach (var frameRate in supportedFrameRates)
-            {
-                Debug.Log($"  supportedFrameRate: {frameRate.FramesPerSecond}");
-            }
+//    private void _watcher_EyeGazeTrackerRemoved(object sender, EyeGazeTracker e)
+//    {
+//        Debug.Log("EyeGazeTracker removed");
+//        _eyeGazeTracker = null;
+//    }
 
-            // Set to second highest framerate, it is 60FPS at this time (30, 60, 90)
-            _eyeGazeTracker.SetTargetFrameRate(supportedFrameRates[supportedFrameRates.Count - 3]);
-            _eyeGazeTrackerNode = SpatialGraphNode.FromDynamicNodeId(e.TrackerSpaceLocatorNodeId);
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError("Unable to open EyeGazeTracker\r\n" + ex.ToString());
-        }
-    }
+//    private async void _watcher_EyeGazeTrackerAdded(object sender, EyeGazeTracker e)
+//    {
+//        Debug.Log("EyeGazeTracker added");
+//        try
+//        {
+//            await e.OpenAsync(true);
+//            _eyeGazeTracker = e;
+//            var supportedFrameRates = _eyeGazeTracker.SupportedTargetFrameRates;
+//            foreach (var frameRate in supportedFrameRates)
+//            {
+//                Debug.Log($"  supportedFrameRate: {frameRate.FramesPerSecond}");
+//            }
 
-#if ENABLE_WINMD_SUPPORT
-    /// <summary>
-    /// Triggers a prompt to let the user decide whether to permit using eye tracking 
-    /// </summary>
-    private async System.Threading.Tasks.Task<bool> AskForEyePosePermission()
-    {
-        var accessStatus = await Windows.Perception.People.EyesPose.RequestAccessAsync();
-        Debug.Log("Eye gaze access status: " + accessStatus.ToString());
-        return accessStatus == Windows.UI.Input.GazeInputAccessStatus.Allowed;
-    }
-#endif
+//            // Set to second highest framerate, it is 60FPS at this time (30, 60, 90)
+//            _eyeGazeTracker.SetTargetFrameRate(supportedFrameRates[supportedFrameRates.Count - 3]);
+//            _eyeGazeTrackerNode = SpatialGraphNode.FromDynamicNodeId(e.TrackerSpaceLocatorNodeId);
+//        }
+//        catch (Exception ex)
+//        {
+//            Debug.LogError("Unable to open EyeGazeTracker\r\n" + ex.ToString());
+//        }
+//    }
 
-    private static UnityEngine.Vector3 ToUnity(System.Numerics.Vector3 v) => new UnityEngine.Vector3(v.X, v.Y, -v.Z);
-}
+//#if ENABLE_WINMD_SUPPORT
+//    /// <summary>
+//    /// Triggers a prompt to let the user decide whether to permit using eye tracking 
+//    /// </summary>
+//    private async System.Threading.Tasks.Task<bool> AskForEyePosePermission()
+//    {
+//        var accessStatus = await Windows.Perception.People.EyesPose.RequestAccessAsync();
+//        Debug.Log("Eye gaze access status: " + accessStatus.ToString());
+//        return accessStatus == Windows.UI.Input.GazeInputAccessStatus.Allowed;
+//    }
+//#endif
+
+//    private static UnityEngine.Vector3 ToUnity(System.Numerics.Vector3 v) => new UnityEngine.Vector3(v.X, v.Y, -v.Z);
+//}
